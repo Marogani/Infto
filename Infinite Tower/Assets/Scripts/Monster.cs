a9e07f4c5e98f28ee7b02419a39557337f9a8bbd
Marogani <rladnwnd4968@naver.com>
2015-07-28 14:21:29 +0900

    Unity test1

---
+++ Infinite Tower/Assets/Scripts/Monster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+	public Transform target;
+	public float enemySpeed = 2;
+
+	void Start () {
+		///target = GameObject.Find("player").transform;
+	}
+	
+	void Update () {
+		Move();
+	}
+	
+	void Move () {
+		/*Vector3 targetDirection = target.position - transform.position;
+		transform.position += targetDirection * enemySpeed * Time.deltaTime;*/
+	}
+	void OnTriggerStay2D(Collider2D other){
+			Vector3 targetDirection = target.position - transform.position;
+			transform.position += targetDirection * enemySpeed * Time.deltaTime;
+	}
+}
---
+++ Infinite Tower/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public int speed = 5;
+	
+	void Update() {
+		Move ();
+	}
+
+	void Move() {
+		var amtMove = speed * Time.deltaTime;
+		var ver = Input.GetAxis("Vertical"); 
+		var hor = Input.GetAxis("Horizontal");
+		
+		transform.Translate (Vector3.right * speed * hor * Time.deltaTime);
+		transform.Translate (Vector3.up * speed * ver * Time.deltaTime);
+
+	}
+}
